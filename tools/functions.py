8a0d75e33de501ad3397a15fe7d45fcc8a1a7126
Leo Irakliotis <lgreco@gmail.com>
2026-09-21 14:13:21 +0000

    mon

---
+++ tools/functions.py
@@ -0,0 +1,49 @@
+def m():
+    print("M     M")
+    print("MM   MM")
+    print("M M M M")
+    print("M  M  M")
+    print("M     M")
+    print()
+def i():
+    print("I")
+    print("I")
+    print("I")
+    print("I")
+    print("I")
+    print()
+def s():
+    print(" SSSSSS")
+    print("S      ")
+    print(" SSSSS ")
+    print("      S")
+    print("SSSSSS ")
+    print()
+def p():
+    print("PPPPPP ")
+    print("P     P")
+    print("PPPPPP")
+    print("P")
+    print("P")
+    print()
+
+def s2():
+    s()
+    s()
+def s2i():
+    s2()
+    i()
+def s2i2():
+    s2i()
+    s2i()
+def p2():
+    p()
+    p()
+
+# --------------------------------
+
+m()
+i()
+s2i2()
+p2()
+i()
